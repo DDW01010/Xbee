36edfdac8760cfc2c5983e45c8532bbf26db1da5
DDW01010 <167614447+DDW01010@users.noreply.github.com>
2024-05-31 19:52:43 +0200

    xbeefinal.ino
    Ik heb de .ino file aangepast zodat ik een header en een implementatie file kan gebruiken.

--- xbeefinal.ino
+++ xbeefinal.ino
@@ -2,46 +2,15 @@
 #include <Zumo32U4Buttons.h>
 #include <Wire.h>
 Zumo32U4Motors motoren;
-
-
-//initialiseer de te gebruiken variabelen voor deze code
-
-int data;
-String data2;
-int speed2 = 100;
-
+using namespace std;
 void setup() {
-  Serial1.begin(9600);
-  Serial.begin(9600);
+Serial1.begin(9600);
+Xbee xbee;
 }
 
 void loop() {
-  //checkt of er een serieel bericht binnen komt vanuit de Xbee, zo ja dan wordt dit opgeslagen in de variabele genaamd data
-  if (Serial1.available() > 0) {
-    data = Serial1.read();
+  xbee.leesWaarde();
+  xbee.converteer();
+/* Ik probeer deze "main" zo klein mogelijk te houden */
 
-    //Wanneer de data die binnenkomt via de Xbee overeen komt met 65(hard naar links), 97(links), 119(voorwaarts), 87(snel voorwaarts), 115(achterwaarts), 83(snel achterwaarts), 100(rechts), 87(hard naar rechts) reageren de motoren hierop door in beweging te komen en reset elke keer weer de motoren zodat de Zumo niet door blijft rijden.
-    if (data == 65) {
-      motoren.setRightSpeed(400);
-    }
-
-    else if (data == 97) {
-      motoren.setRightSpeed(150);
-    } else if (data == 119) {
-      motoren.setSpeeds(150, 150);
-
-    } else if (data == 100) {
-      motoren.setLeftSpeed(150);
-    } else if (data == 87) {
-      motoren.setSpeeds(400, 400);
-    } else if (data == 68) {
-      motoren.setLeftSpeed(400);
-    } else if (data == 115) {
-      motoren.setSpeeds(-150, -150);
-    } else if (data == 83) {
-      motoren.setSpeeds(-400, -400);
-    } else {
-      motoren.setSpeeds(0, 0);
-    }
-  }
 }
